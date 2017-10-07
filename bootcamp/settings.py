63556efb9617d7cf2c6913c50423356075dd6ec9
Kaustav Banerjee <kaustavsmailbox21@gmail.com>
2017-10-08 05:09:55 +0530

    Update settings.py

--- bootcamp/settings.py
+++ bootcamp/settings.py
@@ -23,6 +23,7 @@ ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 # Application definition
 
 INSTALLED_APPS = (
+    'django.contrib.admin'
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
