dae040d19ec299b27c9486e7008e5eeb5dc6d46f
kaustav1996 <kaustavsmailbox21@gmail.com>
2017-10-16 03:15:33 +0530

    User manual Addition done

--- bootcamp/urls.py
+++ bootcamp/urls.py
@@ -1,4 +1,4 @@
-x='olaamigo'
+x='asdf'
 from django.conf import settings
 from django.conf.urls import include, url
 from django.conf.urls.static import static
--- change_signup.py
+++ change_signup.py
@@ -1,6 +1,59 @@
 import re
-with open("./bootcamp/urls.py", "r") as sources:
-    lines = sources.readlines()
-with open("./bootcamp/urls.py", "w") as sources:
-    for line in lines:
-        sources.write(re.sub(r'signup_for_hostel', 'ola', line))
+import shutil
+from tempfile import mkstemp
+
+
+def sed(pattern, replace, source, dest=None, count=0):
+    """Reads a source file and writes the destination file.
+
+    In each line, replaces pattern with replace.
+
+    Args:
+        pattern (str): pattern to match (can be re.pattern)
+        replace (str): replacement str
+        source  (str): input filename
+        count (int): number of occurrences to replace
+        dest (str):   destination filename, if not given, source will be over written.        
+    """
+
+    fin = open(source, 'r')
+    num_replaced = count
+
+    if dest:
+        fout = open(dest, 'w')
+    else:
+        fd, name = mkstemp()
+        fout = open(name, 'w')
+
+    for line in fin:
+        out = re.sub(pattern, replace, line)
+        fout.write(out)
+
+        if out != line:
+            num_replaced += 1
+        if count and num_replaced > count:
+            break
+    try:
+        fout.writelines(fin.readlines())
+    except Exception as E:
+        raise E
+
+    fin.close()
+    fout.close()
+
+    if not dest:
+        shutil.move(name, source)
+f=open('./bootcamp/urls.py','r')
+s=f.readline()
+print("Currently: localhost/"+s.split("'")[1]+"/                               [press CTRL + C if you don't want to change!!]")
+f.close()
+print("\n")
+x=raw_input("Enter New Signup Keyword: ")
+try:
+    sed(s.split("'")[1], x, "./bootcamp/urls.py")
+except WindowsError, e:
+    print("[%] Changing Url")
+f=open('./bootcamp/urls.py','r')
+s=f.readline()
+print("Currently: localhost/"+s.split("'")[1]+"/")
+f.close()
