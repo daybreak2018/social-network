e1c76dc5bb3d55c20b9a92485ce0b0e050562681
Kaustav Banerjee <kaustavsmailbox21@gmail.com>
2017-11-14 16:37:42 +0530

    removed ticklist

--- bootcamp/ticklist/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url
-
-from . import views
-
-urlpatterns = [
-    url(r'^$', views.ticklist, name='ticklist'),
-    url(r'^edit/$',views.EditTickList.as_view(), name='edit'),
-]
